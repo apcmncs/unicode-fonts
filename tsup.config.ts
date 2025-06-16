b3ef6daab1e1926b43c76b688c069b4355aef53b
apcmncs <apcmncs@gmail.com>
2025-06-16 09:54:03 +0700

    feat: update version to 0.0.3, add .github workflows for NPM package publishing, and modify tsup config for conditional minification

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-	entry: ["src/index.ts"],
-	format: ["cjs", "esm"],
-	dts: true,
-	clean: true,
-	minify: true,
+export default defineConfig((options) => {
+	return {
+		entry: ["src/index.ts"],
+		format: ["cjs", "esm"],
+		dts: true,
+		minify: !options.watch,
+		splitting: false,
+		sourcemap: true,
+		clean: true,
+	};
 });
